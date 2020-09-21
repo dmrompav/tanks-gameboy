6bdf4f753876d42f31d18491b358b0a9ce3dee64
pavlovportfolio <dm.rom.pav@gmail.com>
2020-09-21 15:23:17 +0500

    enemies

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const u = document.querySelector('.up'),
 	shot = document.querySelector('.shot');
 
 
-// *Elements ======================================
+// * 1. Elements ======================================
 //Map ------------------
 var row = document.querySelectorAll('.row'),
 	a = [];
@@ -19,25 +19,28 @@ var tank = {
 	y: 9,
 	nav: 12,  //12-3-6-9
 	speed: 0,
-	permission: true,
+	permission: true, //shoot permission or bomb on a field
 	bomb: [],
-};
+},
+	enemy = [
+	],
+	quantity = 0;
 
-//*Process ===========================================
-MapDraw();											//Let start
-u.addEventListener('mousedown', KeyMoveUp);			//Click listeners
+
+//* 2. Process ===========================================
+TankDraw(tank);											//Let start
+u.addEventListener('mousedown', KeyMoveUp);				//Click listeners
 r.addEventListener('mousedown', KeyMoveRight);
 d.addEventListener('mousedown', KeyMoveDown);
 l.addEventListener('mousedown', KeyMoveLeft);
 shot.addEventListener('click', KeyShot);
 document.addEventListener('keydown', KeyMove);
-setInterval(Move, 150);								//FPS
-setInterval(BombMove, 50);
+var moveInt = setInterval(Move, 150),					//FPS
+	bombMoveInt = setInterval(BombMove, 100),
+	enemyMoveInt = setInterval(EnemyMove, 450),
+	enemyCreateInt = setInterval(EnemyCreate, 3000);	//Timer
 
-//*Functions ========================================
-function MapDraw() {														//Drawing -----------
-	TankDraw(tank);
-}
+//* 3.1. Global or player's functions========================================
 function TankClear(tank) {
 	a[tank.y - 1][tank.x].style.backgroundColor = "#777777";
 	a[tank.y][tank.x - 1].style.backgroundColor = "#777777";
@@ -182,7 +185,46 @@ function BombMove() {
 }
 
 
-
+//* 3.2 Enemy functions ===============================================
+function EnemyCreate() {
+	if (quantity < 20) {
+		enemy[quantity] = {
+			x: 1,
+			y: 1,
+			nav: 6,
+			speed: 1,
+			permission: true,
+			bomb: [],
+		}
+		quantity++;
+		console.log(quantity)
+	}
+}
+function EnemyMove() {
+	for (i = 0; i < enemy.length; i++) {
+		TankClear(enemy[i]);
+		if (enemy[i].nav == 12 && enemy[i].y > 1) {
+			enemy[i].y -= enemy[i].speed;
+		}
+		else if (enemy[i].nav == 3 && enemy[i].x < 8) {
+			enemy[i].x += enemy[i].speed;
+		}
+		else if (enemy[i].nav == 6 && enemy[i].y < 18) {
+			enemy[i].y += enemy[i].speed;
+		}
+		else if (enemy[i].nav == 9 && enemy[i].x > 1) {
+			enemy[i].x -= enemy[i].speed;
+		}
+	}
+	for (i = 0; i < enemy.length; i++) {
+		TankDraw(enemy[i]);
+		let changeNav = Math.floor(Math.random() * 10);
+		if (changeNav > 7 || enemy[i].x == 1 || enemy[i].x == 8 || enemy[i].y == 1 || enemy[i].y == 18) {
+			Nav = Math.floor(Math.random() * 4);
+			enemy[i].nav = (Nav + 1) * 3;
+		}
+	}
+}
 
 
 
